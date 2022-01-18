5d9e6d488f7471c6576f43383426242962e90f2a
Manuela Garcia <mgh9517@gmail.com>
2022-01-18 18:35:50 -0500

    Remove unnecessary imports

--- src/screens/Quiz.js
+++ src/screens/Quiz.js
@@ -1,58 +1,55 @@
-import React, { Component, useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { getQuestions } from '../quiz/getQuestions'
 import { capitalizeFirstLetter } from '../helpers/style_string'
 import { resultBank } from '../quiz/resultBank'
 
-function Quiz () {
+function Quiz() {
   const [answer, setAnswer] = useState([])
   const [question, setQuestion] = useState(0)
   const questionBank = getQuestions()
 
-  function handleClick (e) {
+  function handleClick(e) {
     e.preventDefault()
     setAnswer((prevAnswer) => [...prevAnswer, e.target.value])
-    setQuestion(prevQuestion => ++prevQuestion)
+    setQuestion((prevQuestion) => ++prevQuestion)
   }
 
-  function handleReset () {
+  function handleReset() {
     setQuestion(0)
     setAnswer([])
   }
 
   return (
-    <div className='quiz-container'>
-      {question < 4
-        ? (
-          <div>
-            <h1 className='question'>{questionBank[question].questionText}</h1>
-            <button
-              type='button'
-              name='button1'
-              value={questionBank[question].questionAnswer[0]}
-              onClick={(e) => handleClick(e)}
-            >{capitalizeFirstLetter(questionBank[question].questionAnswer[0])}
-            </button>
-            <button
-              type='button'
-              name='button2'
-              value={questionBank[question].questionAnswer[1]}
-              onClick={(e) => handleClick(e)}
-            >{capitalizeFirstLetter(questionBank[question].questionAnswer[1])}
-            </button>
-          </div>
-          )
-        : (
-          <div>
-            <p>{resultBank(answer).score}</p>
-            <p>{resultBank(answer).text}</p>
-            <button
-              type='button'
-              name='reset'
-              onClick={() => handleReset()}
-            >Reiniciar
-            </button>
-          </div>
-          )}
+    <div className="quiz-container">
+      {question < 4 ? (
+        <div>
+          <h1 className="question">{questionBank[question].questionText}</h1>
+          <button
+            type="button"
+            name="button1"
+            value={questionBank[question].questionAnswer[0]}
+            onClick={(e) => handleClick(e)}
+          >
+            {capitalizeFirstLetter(questionBank[question].questionAnswer[0])}
+          </button>
+          <button
+            type="button"
+            name="button2"
+            value={questionBank[question].questionAnswer[1]}
+            onClick={(e) => handleClick(e)}
+          >
+            {capitalizeFirstLetter(questionBank[question].questionAnswer[1])}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p>{resultBank(answer).score}</p>
+          <p>{resultBank(answer).text}</p>
+          <button type="button" name="reset" onClick={() => handleReset()}>
+            Reiniciar
+          </button>
+        </div>
+      )}
     </div>
   )
 }
